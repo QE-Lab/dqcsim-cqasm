b58e96fe500172fe9253cc995aa8a6b89b33faba
Jeroen van Straten <jeroen.van.straten@gmail.com>
2020-01-30 10:23:45 +0100

    Implement all but measure_parity

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <algorithm>
+#include <cctype>
 #include <cstdio>
 #include <ios>
 #include <dqcsim>
@@ -7,6 +9,61 @@
 // Alias the relevant namespaces to something shorter.
 namespace dqcs = dqcsim::wrap;
 namespace cq = compiler;
+
+/**
+ * Used to average measurements over time.
+ */
+class Measurement {
+private:
+  uint64_t num_total = 0;
+  uint64_t num_one = 0;
+  bool latest_value = false;
+
+public:
+  /**
+   * Adds a measurement sample.
+   */
+  void add(bool value) {
+    latest_value = value;
+    num_total++;
+    if (value) num_one++;
+  }
+
+  /**
+   * Returns the estimated probability that the measurement is a one, or -1 if
+   * no estimation is available.
+   */
+  double p1() const {
+    if (!num_total) {
+      return -1.0;
+    }
+    return (double)num_one / (double)num_total;
+  }
+
+  /**
+   * Returns the number of samples taken.
+   */
+  uint64_t num_samples() const {
+    return num_total;
+  }
+
+  /**
+   * Returns the latest measurement.
+   */
+  bool latest() const {
+    return latest_value;
+  }
+
+  /**
+   * Resets averaging.
+   */
+  void reset() {
+    latest_value = false;
+    num_total = 0;
+    num_one = 0;
+  }
+
+};
 
 /**
  * Main plugin class for QX.
@@ -20,9 +77,331 @@ public:
   const std::string filename;
 
   /**
+   * The DQCsim qubits corresponding to the cQASM qubit indices.
+   */
+  std::vector<dqcs::QubitRef> qubits;
+
+  /**
+   * The cQASM classical register.
+   */
+  std::vector<bool> bits;
+
+  /**
+   * The cQASM measurement averaging register.
+   */
+  std::vector<Measurement> measurements;
+
+  /**
    * Constructor.
    */
   CQASMPlugin(std::string &&filename) : filename(filename) {
+  }
+
+  /**
+   * Interprets libQASM's brilliant operation representation into DQCsim's, and
+   * executes it. Returns whether the operation takes time.
+   */
+  bool exec_gate(
+    dqcs::RunningPluginState &state,
+    const cq::Operation &operation
+  ) {
+
+    // Get the operation type.
+    std::string type = operation.getType();
+    DQCSIM_TRACE("operation: %s", type.c_str());
+
+    // Handle conditionals.
+    // NOTE: QX interprets gates with multiple conditionals and multiple qubits
+    // such that all conditions need to be true for each parallel gate, not
+    // each bit controlling each subgate. So we can just get conditions out of
+    // the way immediately.
+    if (operation.isBitControlled()) {
+      for (auto bit_idx : operation.getControlBits().getSelectedBits().getIndices()) {
+        if (!bits[bit_idx]) {
+          return true;
+        }
+      }
+    }
+
+    // Handle prep gates.
+    bool is_prep;
+    dqcs::PauliBasis prep_basis = dqcs::PauliBasis::Z;
+    if (type == "prep_x") {
+      is_prep = true;
+      prep_basis = dqcs::PauliBasis::X;
+    }
+    if (type == "prep_y") {
+      is_prep = true;
+      prep_basis = dqcs::PauliBasis::Y;
+    }
+    if (type == "prep_z") {
+      is_prep = true;
+    }
+    if (is_prep) {
+      dqcs::QubitSet qubit_refs = dqcs::QubitSet();
+      for (auto qubit_idx : operation.getQubitsInvolved().getSelectedQubits().getIndices()) {
+        qubit_refs.push(qubits[qubit_idx]);
+      }
+      state.gate(dqcs::Gate::prep(std::move(qubit_refs), prep_basis));
+      return true;
+    }
+
+    // Handle measurements.
+    bool is_measure = false;
+    bool is_measure_all = false;
+    dqcs::PauliBasis measure_basis = dqcs::PauliBasis::Z;
+    if (type == "measure_x") {
+      is_measure = true;
+      measure_basis = dqcs::PauliBasis::X;
+    }
+    if (type == "measure_y") {
+      is_measure = true;
+      measure_basis = dqcs::PauliBasis::Y;
+    }
+    if (type == "measure_z" || type == "measure") {
+      is_measure = true;
+    }
+    if (type == "measure_all") {
+      is_measure = true;
+      is_measure_all = true;
+    }
+    if (type == "measure_parity") {
+      // FIXME
+      is_measure = true;
+      DQCSIM_ERROR("measure-parity is not implemented! interpreting as measure_z");
+    }
+    if (is_measure) {
+
+      // Get the QX qubit indices.
+      std::vector<size_t> qubit_idxs;
+      if (is_measure_all) {
+        for (size_t qubit_idx = 0; qubit_idx < qubits.size(); qubit_idx++) {
+          qubit_idxs.push_back(qubit_idx);
+        }
+      } else {
+        qubit_idxs = operation.getQubitsInvolved().getSelectedQubits().getIndices();
+      }
+
+      // Convert to DQCsim qubit indices.
+      dqcs::QubitSet qubit_refs = dqcs::QubitSet();
+      for (auto qubit_idx : qubit_idxs) {
+        qubit_refs.push(qubits[qubit_idx]);
+      }
+
+      // Perform the measurement.
+      state.gate(dqcs::Gate::measure(std::move(qubit_refs), measure_basis));
+
+      // Read back the measurement results.
+      for (auto qubit_idx : qubit_idxs) {
+        bool value = false;
+        switch (state.get_measurement(qubits[qubit_idx]).get_value()) {
+          case dqcs::MeasurementValue::Zero: value = false; break;
+          case dqcs::MeasurementValue::One: value = true; break;
+          case dqcs::MeasurementValue::Undefined:
+            DQCSIM_WARN("Received undefined measurement for qubit %d, interpreting as 0", qubit_idx);
+            value = false;
+            break;
+        }
+        bits[qubit_idx] = value;
+        measurements[qubit_idx].add(value);
+      }
+      return true;
+    }
+
+    // Handle reset_averaging.
+    if (type == "reset-averaging") {
+      for (auto &measurement : measurements) {
+        measurement.reset();
+      }
+      return false;
+    }
+
+    // Handle classical NOT.
+    if (type == "not") {
+      for (auto bit_idx : operation.getControlBits().getSelectedBits().getIndices()) {
+        bits[bit_idx] = !bits[bit_idx];
+      }
+      return true;
+    }
+
+    // Handle display.
+    if (type == "display") {
+      static bool warned = false;
+      if (!warned) {
+        DQCSIM_WARN("DQCsim frontends cannot display qubit state; interpreting 'display' gates as 'display_binary'.");
+        warned = true;
+      }
+      type = "display_binary";
+    }
+    if (type == "display_binary") {
+      std::vector<size_t> idxs = operation.getControlBits().getSelectedBits().getIndices();
+      if (!idxs.size()) {
+        for (size_t qubit_idx = 0; qubit_idx < qubits.size(); qubit_idx++) {
+          idxs.push_back(qubit_idx);
+        }
+      }
+
+      for (int i : idxs) {
+        if (measurements[i].num_samples()) {
+          DQCSIM_INFO(
+            "b%d: %d; q%d: %.6f (%d samples, latest = %d)",
+            i, (int)bits[i], i,
+            measurements[i].p1(),
+            measurements[i].num_samples(),
+            (int)measurements[i].latest());
+        } else {
+          DQCSIM_INFO(
+            "b%d: %d; q%d: no data",
+            i, (int)bits[i], i);
+        }
+      }
+      return false;
+    }
+
+    // Handle delay.
+    if (type == "wait") {
+      state.advance(operation.getWaitTime());
+
+      // Pretend that this operation doesn't take time; we've already advanced
+      // all we needed to.
+      return false;
+    }
+
+    // Handle pure-quantum gates.
+    dqcs::PredefinedGate predef_gate;
+    int num_targets = 0;
+    bool has_angle = false;
+    if (type == "i") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::I;
+    }
+    if (type == "x") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::X;
+    }
+    if (type == "y") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::Y;
+    }
+    if (type == "z") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::Z;
+    }
+    if (type == "h") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::H;
+    }
+    if (type == "s") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::S;
+    }
+    if (type == "sdag") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::S_DAG;
+    }
+    if (type == "t") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::T;
+    }
+    if (type == "tdag") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::T_DAG;
+    }
+    if (type == "x90") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::RX_90;
+    }
+    if (type == "mx90") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::RX_M90;
+    }
+    if (type == "y90") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::RY_90;
+    }
+    if (type == "my90") {
+      num_targets = 1;
+      predef_gate = dqcs::PredefinedGate::RY_M90;
+    }
+    if (type == "rx") {
+      num_targets = 1;
+      has_angle = true;
+      predef_gate = dqcs::PredefinedGate::RX;
+    }
+    if (type == "ry") {
+      num_targets = 1;
+      has_angle = true;
+      predef_gate = dqcs::PredefinedGate::RY;
+    }
+    if (type == "rz") {
+      num_targets = 1;
+      has_angle = true;
+      predef_gate = dqcs::PredefinedGate::RZ;
+    }
+    if (type == "cr" || type == "crk") {
+      num_targets = 2;
+      has_angle = true;
+      predef_gate = dqcs::PredefinedGate::Phase;
+    }
+    if (type == "swap") {
+      num_targets = 2;
+      predef_gate = dqcs::PredefinedGate::Swap;
+    }
+    if (type == "cnot") {
+      num_targets = 2;
+      predef_gate = dqcs::PredefinedGate::X;
+    }
+    if (type == "cz") {
+      num_targets = 2;
+      predef_gate = dqcs::PredefinedGate::Z;
+    }
+    if (type == "toffoli") {
+      num_targets = 3;
+      predef_gate = dqcs::PredefinedGate::X;
+    }
+    if (num_targets) {
+
+      // Figure out the qubit indices to operate on.
+      auto qubit_idxs = std::vector<std::vector<size_t>>();
+      if (num_targets == 1) {
+        qubit_idxs.push_back(operation.getQubitsInvolved().getSelectedQubits().getIndices());
+      } else {
+        for (int i = 1; i <= num_targets; i++) {
+          qubit_idxs.push_back(operation.getQubitsInvolved(i).getSelectedQubits().getIndices());
+        }
+      }
+
+      // Fail if the args have different sizes.
+      for (int i = 1; i < num_targets; i++) {
+        if (qubit_idxs[0].size() != qubit_idxs[i].size()) {
+          throw std::runtime_error("Gate has differently-sized qubit argument ranges");
+        }
+      }
+
+      // Execute the parallel gates.
+      for (auto g = 0; g < qubit_idxs[0].size(); g++) {
+
+        // Figure out the predefined gate parameters.
+        dqcs::ArbData params = dqcs::ArbData();
+        if (has_angle) {
+          params.with_arg(operation.getRotationAngle());
+        }
+
+        // Construct the qubit set.
+        dqcs::QubitSet qubit_refs = dqcs::QubitSet();
+        for (int i = 0; i < num_targets; i++) {
+          qubit_refs.push(qubits[qubit_idxs[i][g]]);
+        }
+
+        // Send the gate.
+        state.gate(dqcs::Gate::predefined(predef_gate, std::move(qubit_refs), std::move(params)));
+
+      }
+
+      return true;
+    }
+
+    throw std::runtime_error("Unsupported gate: " + type);
   }
 
   /**
@@ -38,8 +417,12 @@ public:
     std::fclose(fp);
     auto cqasm = sm.getQasmRepresentation();
 
-    // Allocate qubits downstream.
-    auto qubits = state.allocate(cqasm.numQubits()).drain_into_vector();
+    // Allocate qubits, bits, and measurements.
+    qubits = state.allocate(cqasm.numQubits()).drain_into_vector();
+    for (auto i = 0; i < cqasm.numQubits(); i++) {
+      bits.emplace_back(false);
+      measurements.emplace_back();
+    }
 
     // Arb the error model downstream in case we're using QX.
     // TODO
@@ -56,12 +439,13 @@ public:
           iter,
           circuit.nameSubCircuit().c_str());
         for (auto bundle : circuit.getOperationsCluster()) {
-          size_t delay = 1;
+          bool timed = false;
           for (auto op : bundle->getOperations()) {
-            // TODO
-            DQCSIM_TRACE("gate: %s", op->getType().c_str());
+            timed |= exec_gate(state, *op);
           }
-          state.advance(delay);
+          if (timed) {
+            state.advance(1);
+          }
         }
       }
     }
@@ -70,6 +454,9 @@ public:
     for (auto qubit : qubits) {
       state.free(qubit);
     }
+    qubits.clear();
+    bits.clear();
+    measurements.clear();
 
     return args;
   }
